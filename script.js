1a9c01b0df7f8d5d7cf791204949a3f61e75e87b
Maréme Thiaw <mariamathiaw92@gmail.com>
2025-05-10 14:21:05 +0100

    Retouche du footer, rendre responsive le menu

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/* Menu Burguer */
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links a'); /* Permet au menu de se refermer aprés un click sur un lien */
+  
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      navLinks.classList.toggle('active');
+    });
+
+     // Fermer le menu et retirer la croix après clic sur un lien
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        burger.classList.remove('active');
+      });
+    });
+  });
+/* Mail de contact */
 (function(){
     emailjs.init("Cr7OYFwbeYlnYZqfO"); // Ta clé publique
   })();
